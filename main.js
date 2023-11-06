243673eaac08dfbf1ed162cc93ed8a654f7fc73c
Gorcea Radu Andrei <80151170+lamoo7@users.noreply.github.com>
2023-11-06 22:49:00 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -13,32 +13,27 @@
     setTimeout(slide, 3000); 
   }
   
-  // Function to check if at least 50% of an element is in the viewport
-function isElementInViewport(element) {
-  const rect = element.getBoundingClientRect();
-  const windowHeight = (window.innerHeight || document.documentElement.clientHeight);
-  return (
-    (rect.top <= 0 && rect.bottom >= 0) ||
-    (rect.top >= 0 && rect.bottom <= windowHeight) ||
-    (rect.top <= windowHeight && rect.bottom >= windowHeight)
-  );
-}
-
-// Function to update the active link based on the section in the viewport
-function updateActiveLink() {
-  const sections = document.querySelectorAll('.section');
   const menuLinks = document.querySelectorAll('.menu-link');
+      const sections = document.querySelectorAll('.section');
 
-  sections.forEach((section, index) => {
-    if (isElementInViewport(section)) {
-      menuLinks.forEach((link) => link.classList.remove('active'));
-      menuLinks[index].classList.add('active');
-    }
-  });
-}
+      function isElementInMiddle(element) {
+        const elementRect = element.getBoundingClientRect();
+        const middleOfScreen = window.innerHeight / 2;
 
-// Add an event listener to update the active link when scrolling
-window.addEventListener('scroll', updateActiveLink);
+        // Calculate the middle position of the element
+        const elementMiddle = elementRect.top + elementRect.height / 2;
 
-// Call the updateActiveLink function when the page loads to set the initial active link
-window.addEventListener('load', updateActiveLink);
+        return Math.abs(elementMiddle - middleOfScreen) < elementRect.height / 2;
+      }
+
+      function updateActiveLink() {
+        sections.forEach((section, index) => {
+          if (isElementInMiddle(section)) {
+            menuLinks.forEach((link) => link.classList.remove('active'));
+            menuLinks[index].classList.add('active');
+          }
+        });
+      }
+
+      window.addEventListener('scroll', updateActiveLink);
+      window.addEventListener('load', updateActiveLink);
